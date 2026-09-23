787cc87a402fb1b7424fc7ce041d751e18ec149a
maddulaseetasravankumar <maddulaseetasravankumar@gmail.com>
2026-09-23 19:10:41 +0530

    Improve todo app UI and responsive design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
+import "./App.css";
 import Form from "./components/Form";
 
 function App() {
   return (
     <div className="wrapper">
       <h1>Todo List</h1>
-      <Form/>
-      <div className='copy-rights'>
-        <p>Developed by <a href="https://sravankumar.42web.io/">Sravan Kumar</a></p>
-         </div>
+      <Form />
+      <div className="copy-rights">
+        <p>
+          Developed by{" "}
+          <a href="https://maddula-sravan-kumar.vercel.app/">Sravan Kumar</a>
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,125 +1,147 @@
-import React from 'react';
-import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
-import BookmarkIcon from '@material-ui/icons/Bookmark';
+import React from "react";
+
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
+
+import List from "@material-ui/core/List";
+
+import ListItem from "@material-ui/core/ListItem";
+
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+
+import ListItemText from "@material-ui/core/ListItemText";
+
+import Checkbox from "@material-ui/core/Checkbox";
+
+import IconButton from "@material-ui/core/IconButton";
+
+import EditIcon from "@material-ui/icons/Edit";
+
+import DeleteIcon from "@material-ui/icons/Delete";
+
+import BookmarkIcon from "@material-ui/icons/Bookmark";
 
 const useStyles = makeStyles(() => ({
-    root: {
-        width: '100%',
-        backgroundColor: '#f5f5f5',
-        padding: 0,
-    },
-    li: {
-        borderBottom: '1px dashed black'
-    }
+  root: {
+    width: "100%",
+
+    padding: 0,
+
+    backgroundColor: "transparent",
+  },
 }));
 
-const TodoList = ({ theme, todos, completeTodo, editTodo, deleteTodo, saveTodo, noteRef, preventSubmit }) => {
-    const classes = useStyles();
-    const [checked, setChecked] = React.useState([0]);
-    let UniqKey = 123;
+const TodoList = ({
+  theme,
+  todos,
+  completeTodo,
+  editTodo,
+  deleteTodo,
+  saveTodo,
+  noteRef,
+  preventSubmit,
+}) => {
+  const classes = useStyles();
 
+  return (
+    <ThemeProvider theme={theme}>
+      <List className={classes.root}>
+        {todos.map((todo, inx) => {
+          const labelId = `list-todo-${inx}`;
 
-    const handleToggle = (value, inx) => () => {
-        const currentIndex = checked.indexOf(value);
-        const newChecked = [...checked];
+          return (
+            <ListItem
+              key={`todo-${inx}`}
+              className={`todo-item ${
+                todo.isCompleted ? "todo-completed" : ""
+              }`}
+              role={undefined}
+              dense
+            >
+              {/* =========================
+                  CHECKBOX
+              ========================== */}
 
-        if (currentIndex === -1) {
-            newChecked.push(value);
-        } else {
-            newChecked.splice(currentIndex, 1);
-        }
+              <ListItemIcon>
+                <Checkbox
+                  className="todo-checkbox"
+                  color="primary"
+                  edge="start"
+                  checked={Boolean(todo.isCompleted)}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{
+                    "aria-labelledby": labelId,
+                  }}
+                  onChange={() => completeTodo(inx)}
+                  onKeyPress={preventSubmit}
+                />
+              </ListItemIcon>
 
-        setChecked(newChecked);
-        completeTodo(inx);
-    };
+              {/* =========================
+                  TODO TEXT / EDIT INPUT
+              ========================== */}
 
+              {!todo.isEditing ? (
+                <ListItemText
+                  className="todo-text"
+                  id={labelId}
+                  primary={todo.text}
+                />
+              ) : (
+                <>
+                  <label htmlFor={`task-${inx}`} className="visuallyhidden">
+                    {todo.text}
+                  </label>
 
-    return (
-        <ThemeProvider theme={theme}>
-            <List className={classes.root}>
-            {todos.map((todo, inx) => {
-                const labelId = `list-todo-${todo}`;
+                  <input
+                    id={`task-${inx}`}
+                    className="form__edit-input"
+                    defaultValue={todo.text}
+                    ref={(element) => {
+                      noteRef.current[inx] = element;
+                    }}
+                    onKeyPress={preventSubmit}
+                  />
+                </>
+              )}
 
-                return (
-                    <ListItem
-                        key={`todo-${UniqKey++}`}
-                        role={undefined}
-                        dense
-                        button
-                        className={classes.li}
-                    >
-                        <ListItemIcon>
-                            <Checkbox
-                                color="primary"
-                                edge="start"
-                                checked={checked.indexOf(todo) !== -1}
-                                tabIndex={-1}
-                                disableRipple
-                                inputProps={{ 'aria-labelledby': labelId }}
-                                onClick={handleToggle(todo, inx)}
-                                onKeyPress={preventSubmit}
-                            />
-                        </ListItemIcon>
-                        {
-                            (!todo.isEditing) ?
-                                <>
-                                    <ListItemText
-                                        id={labelId}
-                                        primary={`${todo.text}`}
-                                        style={{textDecoration: todo.isCompleted ? "line-through" : ""}}
-                                    />
-                                    <ListItemIcon>
-                                        <IconButton
-                                            edge="end"
-                                            aria-label="edit"
-                                            onClick={() => editTodo(inx)}
-                                        >
-                                            <EditIcon/>
-                                        </IconButton>
-                                    </ListItemIcon>
-                                </>
-                                :
-                                <>
-                                    <label
-                                        htmlFor="task" // better accessibility with HTML
-                                        className="visuallyhidden"
-                                    >
-                                        {todo.text}
-                                    </label>
-                                    <input
-                                        className="form__edit-input"
-                                        defaultValue={todo.text}
-                                        ref={(element) => noteRef.current[inx] = element}
-                                        onKeyPress={preventSubmit}
-                                        id="task"
-                                    />
-                                    <ListItemIcon>
-                                        <IconButton onClick={() => saveTodo(inx)} edge="end" aria-label="delete">
-                                            <BookmarkIcon />
-                                        </IconButton>
-                                    </ListItemIcon>
-                                </>
-                        }
-                        <ListItemSecondaryAction>
-                            <IconButton onClick={() => deleteTodo(inx)} edge="end" aria-label="delete">
-                                <DeleteIcon />
-                            </IconButton>
-                        </ListItemSecondaryAction>
-                    </ListItem>
-                );
-            })}
-        </List>
-        </ThemeProvider>
-    );
-}
+              {/* =========================
+                  ACTION BUTTONS
+              ========================== */}
+
+              <div className="todo-actions">
+                {!todo.isEditing ? (
+                  <IconButton
+                    className="todo-action-button"
+                    aria-label="edit"
+                    onClick={() => editTodo(inx)}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                ) : (
+                  <IconButton
+                    className="todo-action-button"
+                    aria-label="save"
+                    onClick={() => saveTodo(inx)}
+                  >
+                    <BookmarkIcon />
+                  </IconButton>
+                )}
+
+                <IconButton
+                  className="todo-action-button"
+                  aria-label="delete"
+                  onClick={() => deleteTodo(inx)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </div>
+            </ListItem>
+          );
+        })}
+      </List>
+    </ThemeProvider>
+  );
+};
 
 export default TodoList;
